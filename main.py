2e9ff9e9e0e7aa190d2e31526b737412ece97aed
Vladimir Kerdiak <vladimir.kerdiak@gmail.com>
2022-03-25 13:01:27 +0200

    initial

---
+++ main.py
@@ -0,0 +1,14 @@
+from PIL import Image
+
+def cut_image_by_quarter(path):
+    if not isinstance(path, str):
+        raise Exception("There should be a string")
+
+    image = Image.open(path)
+    width = image.size[0]
+    heigth = image.size[1]
+    new_image = image.resize((int(width*0.75), int(heigth*0.75)))
+    path_cropped = "image/cropped/cropped.jpg"
+    new_image.save(path_cropped)
+
+cut_image_by_quarter("image/original/image.jpg")
